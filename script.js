6c6163dc880cbb2de4e281e91787794169f1e364
pranjalkatiyar <pranjalkatiyar23@gmail.com>
2021-09-19 13:24:10 +0530

    js for the nabvbar active with  the for each,loader function with fading,loader css and fadeout css,foodloader gif uploaded

--- script.js
+++ script.js
@@ -7,10 +7,30 @@ menu.onclick = () =>{
 
 }
 
+let section=document.querySelectorAll('section');
+let navLinks=document.querySelectorAll('header .navbar a');
+
+
+
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 
+    section.forEach(sec =>{
+
+      let top = window.scrollY;
+      let height = sec.offsetHeight;
+      let offset = sec.offsetTop - 150;
+      let id = sec.getAttribute('id');
+  
+      if(top >= offset && top < offset + height){
+        navLinks.forEach(links =>{
+          links.classList.remove('active');
+          document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+        });
+      };
+  
+    });
 }
 
 document.querySelector('#search-icon').onclick = () =>{
@@ -63,3 +83,13 @@ var swiper = new Swiper(".home-slider", {
             }
     }
   });
+
+
+  function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+  }
+  function fadeOut(){
+    setInterval(loader,3000);
+  }
+
+  window.onload = fadeOut;
